e507220a4a16b9c06626786b50072bb769c338c4
Brendan Fuller <Import-Python@users.noreply.github.com>
2017-04-30 09:59:26 -0500

    ZLogger Lib/Class Upload

---
+++ ZLogger.java
@@ -0,0 +1,50 @@
+package org.usfirst.frc2337.libraries;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+/***
+ * ZLogger: a FRC based logging system that runs on the DriverStatiom 
+ * @author Import-Python (OnoUtilities)
+ *
+ */
+public class ZLogger {
+
+	public String nt = "ZLogger"; //Main Table
+	public NetworkTable table; //NT when starts
+	public ZLogger() {
+		table = NetworkTable.getTable(nt);
+	}
+	/***
+	 * setEnabled()
+	 * Enabled State of the Robot
+	 */
+	public void setEnabled() {
+		table.putBoolean(nt + "/status", true);
+	}
+	/***
+	 * setAutonEnabled()
+	 * Enabled State of the Robot and make State to 'auton' 
+	 */
+	public void setAutonEnabled() {
+		setEnabled();
+		table.putString(nt + "/state", "auton");
+	}
+	/***
+	 * setTelopEnabled()
+	 * Enabled State of Robot and make State to 't' 
+	 */
+	public void setTelopEnabled() {
+		setEnabled();
+		table.putString(nt + "/state", "teleop");
+	}
+	/***
+	 * setTelopEnabled()
+	 * Disables the Robot  
+	 */
+	public void setDisabled() {
+		table.putString(nt + "/state", "disabled");
+	}
+	public void sendDebug(String msg) {
+		table.putString(nt + "/state", msg);
+	}
+	
+}
